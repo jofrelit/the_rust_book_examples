65c44056a24673b233384e4cdca86786999ccfcb
Johan Liljeqvist <johan@jofrelit.net>
2022-07-26 11:24:59 +0200

    Morte testing of enums and matching

--- ch06-00-enums-and-pattern-matchning/src/main.rs
+++ ch06-00-enums-and-pattern-matchning/src/main.rs
@@ -1,18 +1,38 @@
-#[derive(Debug)]
+use std::fmt;
+
 enum UsState {
     Alabama,
     Alaska,
 }
 
+impl fmt::Display for UsState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let typename = match self {
+            UsState::Alabama => "Alabama",
+            UsState::Alaska => "Alaska",
+        };
+        write!(f, "{}", typename)
+    }
 
-#[derive(Debug)]
+}
+
 enum UsCoin {
     Penny,
     Nickel,
     Dime,
-    Quarter(UsState),
+    Quarter(Option<UsState>),
 }
-
+impl fmt::Display for UsCoin {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let typename = match self {
+            UsCoin::Penny => "Penny",
+            UsCoin::Nickel => "Nickel",
+            UsCoin::Dime => "Dime",
+            UsCoin::Quarter(_) => "Quarter",
+        };
+        write!(f, "{}", typename)
+    }
+}
 
 
 fn value_in_cents(coin: &UsCoin) -> u8 {
@@ -27,18 +47,34 @@ fn value_in_cents(coin: &UsCoin) -> u8 {
 
 
 
+
 fn main() {
 
     let coin_array = [
         UsCoin::Penny,
         UsCoin::Nickel,
         UsCoin::Dime,
-        UsCoin::Quarter(UsState::Alabama),
+        UsCoin::Quarter(Some(UsState::Alabama)),
+        UsCoin::Quarter(Some(UsState::Alaska)),
+        UsCoin::Quarter(None),
     ];
 
     for coin in coin_array {
         let coin_value = value_in_cents(&coin);
-        println!("The value of a {:?} is {} cent", coin, coin_value);
+
+        match &coin {
+           UsCoin::Quarter(optional_state) =>  {
+                if let Some(state) = optional_state {
+                    println!("The value of a {} {} is {} cent",state, coin, coin_value);
+                } else {
+                    println!("The value of a {} is {} cent", coin, coin_value); 
+                }
+            }
+            _ => {
+                println!("The value of a {} is {} cent", coin, coin_value);
+            }
+        }
+        
     }
 
 
